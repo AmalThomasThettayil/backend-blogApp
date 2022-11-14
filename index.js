83fc4a04ec4c2ec2b820a3c809aa123cad5bf0f8
AmalThomasThettayil <102983661+AmalThomasThettayil@users.noreply.github.com>
2022-11-14 15:26:52 +0530

    cors express update

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
 
 // cors
-// app.use(cors({
-//     origin: ["https://express-ten-zeta.vercel.app"],
-//     credentials: true,
-// }
-// ))
+app.use(cors({
+    origin: ["https://express-ten-zeta.vercel.app"],
+    credentials: true,
+}
+))
 
-app.use(cors())
+// app.use(cors())
 
 //users route
 app.use("/api", userRoutes)
